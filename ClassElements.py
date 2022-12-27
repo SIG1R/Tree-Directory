04f98e9202b42877697a6727e9104e3e89dddf1d
BlackMyze <grsebas04@gmail.com>
2022-12-26 21:23:37 -0500

    This is the first version of this code... Much thing will be better

---
+++ ClassElements.py
@@ -0,0 +1,71 @@
+import os
+
+class Directory:
+
+    def __init__(self, path, deep = 0, *content):
+        
+        '''
+        self.name -> Name of directory
+        self.path -> Path or URL of directory
+        self.subdirectory/files -> List of directories/files what are in self.path
+        self.size -> Total size of that directoty
+        self.deep -> Distance what are to root folder
+        '''
+        self.name = path.split('/')[-1] 
+        self.path = path
+        self.sub_directories = []
+        self.files = []
+        self.size = None
+        self.deep = deep
+        
+
+        for element in content:
+            assert type(element) is str
+
+            os.chdir(self.path)
+            
+            tempDir = {
+                'newPath': self.path + '/' + element,
+                'newDeep': self.deep + 1,
+            }
+
+            if not('.' in element): 
+ 
+                
+                os.chdir(tempDir['newPath'])
+    
+                try:
+                    self.sub_directories.append(Directory(tempDir['newPath'], tempDir['newDeep'], *os.listdir()))
+
+                except NotADirectoryError:
+                    self.files.append(File(tempDir['newPath'], tempDir['newDeep']))
+
+            else: 
+                self.files.append(File(tempDir['newPath'], tempDir['newDeep']))
+
+    def __str__(self):
+        
+        TAB = '\t'
+        SPACE = '\n'
+        toPrint = self.deep * TAB + self.name + SPACE
+
+        for folder in self.sub_directories:
+            toPrint += Directory.__str__(folder)
+        
+        for file in self.files:
+            toPrint += File.__str__(file)
+
+        return toPrint
+
+class File:
+    
+    def __init__(self, path, deep=0):
+        
+        self.name = path.split('/')[-1]   
+        self.path = path
+        self.size = None
+        self.deep = deep
+
+    def __str__(self):
+
+        return self.deep*'\t'+ self.name + '\n'
